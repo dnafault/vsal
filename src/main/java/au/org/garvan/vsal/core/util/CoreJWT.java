28a1f5fd307a4606cb7a6085b8ad2c2172438487
Dmitry Degrave <d.degrave@garvan.org.au>
2017-09-19 00:15:16 +1000

    jwt claims verification

--- src/main/java/au/org/garvan/vsal/core/util/CoreJWT.java
+++ src/main/java/au/org/garvan/vsal/core/util/CoreJWT.java
@@ -6,13 +6,17 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
 public class CoreJWT {
     public static void verifyJWT(String token)
             throws UnsupportedEncodingException, JWTVerificationException {
-        String key = ReadConfig.getProp().getProperty("jwt.key");
-        Algorithm algorithm = Algorithm.HMAC256(key);
-        JWTVerifier verifier = JWT.require(algorithm).build();
+        Properties p = ReadConfig.getProp();
+        Algorithm algorithm = Algorithm.HMAC256(p.getProperty("jwtKey"));
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer(p.getProperty("jwtIssuer"))
+                .withArrayClaim(p.getProperty("jwtAccessClaim"), p.getProperty("jwtAccessValue"))
+                .build();
         verifier.verify(token);
     }
 }
