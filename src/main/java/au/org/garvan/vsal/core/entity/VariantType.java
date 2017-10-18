56fdad0d11094e07562e945d2e82045bcf44ead9
Dmitry Degrave <d.degrave@garvan.org.au>
2017-10-19 01:22:33 +1100

    types

--- src/main/java/au/org/garvan/vsal/core/entity/VariantType.java
+++ src/main/java/au/org/garvan/vsal/core/entity/VariantType.java
@@ -2,7 +2,7 @@ package au.org.garvan.vsal.core.entity;
 
 public enum VariantType {
 
-    SNV((byte)0), MNV((byte)1), INDEL((byte)2), SV((byte)3), CNV((byte)4);
+    SNV((byte)83), MNV((byte)77), INS((byte)105), DEL((byte)100), INDEL((byte)73), SV((byte)115), CNV((byte)99);
 
     private final Byte type;
 
@@ -13,11 +13,13 @@ public enum VariantType {
     public static VariantType fromByte(Byte type) {
         if (type != null)
             switch (type) {
-                case 0 : return VariantType.SNV;
-                case 1 : return VariantType.MNV;
-                case 2 : return VariantType.INDEL;
-                case 3 : return VariantType.SV;
-                case 4 : return VariantType.CNV;
+                case 83  : return VariantType.SNV;   // S
+                case 77  : return VariantType.MNV;   // M
+                case 105 : return VariantType.INS;   // i
+                case 100 : return VariantType.DEL;   // d
+                case 73  : return VariantType.INDEL; // I
+                case 115 : return VariantType.SV;    // s
+                case 99  : return VariantType.CNV;   // c
             }
         return null;
     }
@@ -37,6 +39,8 @@ public enum VariantType {
                 case "SNV"  : return VariantType.SNV;
                 case "SNP"  : return VariantType.SNV;
                 case "MNV"  : return VariantType.MNV;
+                case "INS"  : return VariantType.INS;
+                case "DEL"  : return VariantType.DEL;
                 case "INDEL": return VariantType.INDEL;
                 case "SV"   : return VariantType.SV;
                 case "CNV"  : return VariantType.CNV;
