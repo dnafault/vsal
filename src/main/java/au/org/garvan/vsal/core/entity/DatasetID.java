2ed7680a296a8331e10f336c0aad097f90235e56
Dmitry Degrave <dmeetry@gmail.com>
2019-12-13 15:47:04 +1100

    new cohorts

--- src/main/java/au/org/garvan/vsal/core/entity/DatasetID.java
+++ src/main/java/au/org/garvan/vsal/core/entity/DatasetID.java
@@ -28,7 +28,8 @@ package au.org.garvan.vsal.core.entity;
 public enum DatasetID {
 
     ASPREE("ASPREE"), MGRB("MGRB"), MITO("MITO"), NEURO("NEUROMUSCULAR"), AC("ACUTECARE"), BM("BM"), EE("EE"),
-    ICCON("ICCON"), LD("LEUKODYSTROPHIES"), DEMO("DEMO"), CIRCA("CIRCA"), CHILDRANZ("CHILDRANZ");
+    ICCON("ICCON"), LD("LEUKODYSTROPHIES"), DEMO("DEMO"), CIRCA("CIRCA"), CHILDRANZ("CHILDRANZ"), CARDIAC("CARDIAC"),
+    GI("GI"), HIDDEN("HIDDEN"), KIDGEN("KIDGEN") ;
 
     private final String dataset;
 
